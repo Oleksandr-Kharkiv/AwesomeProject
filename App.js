0bff987ea6b12fc51e15e5f1c0775e8f719b62f4
Oleksandr-Kharkiv <115045030+Oleksandr-Kharkiv@users.noreply.github.com>
2023-06-08 03:06:14 +0200

    test

--- App.js
+++ App.js
@@ -1,24 +1,15 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, SafeAreaView, Text, View, Button, ScrollView, Image, Switch, TextInput } from 'react-native';
-// import { RegistrationScreen } from './components/RegistrationScreen/RegistrationScreen';
+import { StyleSheet, SafeAreaView, View, Button, ScrollView, Image, Switch, TextInput } from 'react-native';
+import RegistrationScreen from './components/RegistrationScreen/RegistrationScreen';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Roboto': require('./assets/fonts/Roboto-Medium.ttf'),
+  });
   return (
     <View style={styles.container}>
-      <Text>?|?|?|!/\/\/\/</Text>
-      <Button />
-      <ScrollView />
-      <Image />
-      <Switch />
-      <TextInput />
-      {/* <SafeAreaView>
-        <TextInput
-          // style={styles.input}
-          // onChangeText={onChangeText}
-          // value={text}
-        />
-        </SafeAreaView> */}
-      <StatusBar style="auto" />
+      <RegistrationScreen/>
     </View>
   );
 }
--- components/RegistrationScreen/RegistrationScreen.js
+++ components/RegistrationScreen/RegistrationScreen.js
@@ -1,37 +1,85 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+import { View, StyleSheet, TextInput, Text, Button } from "react-native";
 
 const RegistrationScreen = () => {
-  const [text, onChangeText] = React.useState("Useless Text");
+  const [text, onChangeText] = React.useState("");
   const [number, onChangeNumber] = React.useState("");
   return (
-    <View>
-      return (
-      <SafeAreaView>
-        <TextInput
-          style={styles.input}
-          onChangeText={onChangeText}
-          value={text}
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={onChangeNumber}
-          value={number}
-          placeholder="useless placeholder"
-          keyboardType="numeric"
-        />
-      </SafeAreaView>)
+    <View style={styles.form}>
+      <Text style={styles.text}>Реєстрація</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder="Логін"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={text}
+        placeholder="Адреса електронної пошти"
+        // keyboardType="numeric"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="Пароль"
+      />
+      <Button style={styles.button}/>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  // form: {
+  //   position: absolute,
+  //   width: 375,
+  //   height: 549,
+  //   left: 0,
+  //   top: 263,
+  //   backgroundColor: FFFFFF,
+  //   borderRadius: 25,
+  // },
+  text: {
+    textAlign: "center",
+    fontFamily: "Roboto",
+    color: "212121",
+    fontWeight: 500,
+    fontSize: 30,
+    lineHeight: 3.17,
+    letterSpacing: 0.01,
+    marginBottom: 32,
+  },
   input: {
+    backgroundColor: "F6F6F6",
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: "E8E8E8",
+    borderRadius: 8,
     height: 40,
     margin: 12,
     borderWidth: 1,
     padding: 10,
   },
+  // button: {
+    // display: flex,
+    // flexDirection: column,
+    // alignItems: center,
+    // paddingTop: 16,
+    // paddingBottom: 16,
+    // paddingLeft: 32,
+    // paddingRight: 32,
+    // gap: 12,
+    // // position: absolute,
+    // height: 51,
+    // left: 16,
+    // right: 16,
+    // bottom: 113,
+
+    // background: "FF6C00",
+    // borderRadius: 100,
+  // },
 });
 
 export default RegistrationScreen;

3b6f699e5975f14774277deb71e68fcd1ec6f580
Oleksandr <115045030+Oleksandr-Kharkiv@users.noreply.github.com>
2023-08-05 02:36:58 +0200

    add regScreen

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
-import photo_bg from "./Images/photo_bg.jpg";
 import {
   StyleSheet,
   SafeAreaView,
@@ -10,21 +9,16 @@ import {
   Image,
   Switch,
   TextInput,
-  ImageBackground
 } from "react-native";
 import RegistrationScreen from "./Screens/RegistrationScreen/RegistrationScreen";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    Roboto500: require("./assets/fonts/Roboto-Medium.ttf"),
-    Roboto400: require("./assets/fonts/Roboto-Regular.ttf"),
+    'Roboto500': require("./assets/fonts/Roboto-Medium.ttf"),
+    'Roboto400': require("./assets/fonts/Roboto-Regular.ttf"),
   });
   return (
     <View style={styles.container}>
-      <ImageBackground
-        source={photo_bg}
-        style={{ position: "absolute", width: "100%", height: "100%"}}
-      />
         <RegistrationScreen />
     </View>
   );
--- Screens/RegistrationScreen/RegistrationScreen.jsx
+++ Screens/RegistrationScreen/RegistrationScreen.jsx
@@ -1,5 +1,4 @@
-// import homeSvg from "./Images/home_indicator.svg";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Text,
   StyleSheet,
@@ -7,103 +6,207 @@ import {
   Image,
   TextInput,
   TouchableOpacity,
+  ImageBackground,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Platform,
 } from "react-native";
-// import { inline } from "react-native-web/dist/cjs/exports/StyleSheet/compiler";
+import {AntDesign} from '@expo/vector-icons';
+import photo_bg from "../../Images/photo_bg.jpg";
+
 
 const RegistrationScreen = () => {
   const [logIn, setLogIn] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
+  const [isKeyboardOpen, setIsKeyboardOpen] = React.useState(false);
+  const [userPhoto, setUserPhoto] = useState(false);
 
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener(
+      "keyboardDidShow",
+      () => {
+        setIsKeyboardOpen(true);
+      }
+    );
+    const keyboardDidHideListener = Keyboard.addListener(
+      "keyboardDidHide",
+      () => {
+        setIsKeyboardOpen(false);
+      }
+    );
+
+    return () => {
+      keyboardDidShowListener.remove();
+      keyboardDidHideListener.remove();
+    };
+  }, []);
   return (
-    <View style={styles.form}>
-      <View style={styles.textWrap}>
-        <Text style={styles.title}>Реєстрація</Text>
-      </View>
-      <View style={styles.inputsWrap}>
-        <TextInput
-          style={styles.input}
-          placeholder="Логін"
-          onChangeText={setLogIn}
-          value={logIn}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Адреса електронної пошти"
-          onChangeText={setEmail}
-          keyboardType="email-address"
-          value={email}
-        />
-        <View style={styles.passwordInputWrap}>
-          <TextInput
-            style={styles.input}
-            placeholder="Пароль"
-            onChangeText={setPassword}
-            keyboardType="numeric"
-            value={password}
-          />
-          <TouchableOpacity
-            style={styles.showPasswordLink}
-            onPress={() => {
-              console.log("Показати");
-            }}
+    <View style={styles.screen}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ImageBackground source={photo_bg} style={styles.backgroundImg}>
+          <KeyboardAvoidingView
+            style={styles.wrap}
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            // keyboardVerticalOffset={Platform.OS === "ios" ? -170 : -135}
           >
-            <Text style={styles.showPasswordText}>Показати</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => {
-          console.log("Зареєстуватися");
-        }}
-      >
-        <View style={styles.textWrap}>
-          <Text style={styles.buttonTitle}>Зареєстуватися</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.link}
-        onPress={() => {
-          console.log("Вже є акаунт? Увійти");
-        }}
-      >
-        <Text style={styles.linkText}>Вже є акаунт? Увійти</Text>
-      </TouchableOpacity>
-
-      {/* <Image source={homeSvg} /> */}
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+              <View style={styles.form}>
+                <TouchableOpacity style={styles.userImgWrap}>
+                  <AntDesign name={userPhoto ? "closecircleo" : "pluscircleo"}
+                    color={userPhoto ? "black" : "#FF6C00"} 
+                    style={styles.iconControlUserPhoto}
+                    size={25} />
+                </TouchableOpacity>
+                <View style={styles.textWrap}>
+                  <Text style={styles.title}>Реєстрація</Text>
+                </View>
+                <View style={styles.inputsWrap}>
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Логін"
+                    onChangeText={setLogIn}
+                    value={logIn}
+                    autoCompleteType="off"
+                  />
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Адреса електронної пошти"
+                    onChangeText={setEmail}
+                    keyboardType="email-address"
+                    value={email}
+                    autoCompleteType="off"
+                  />
+                  <View style={styles.passwordInputWrap}>
+                    <TextInput
+                      style={styles.input}
+                      placeholder="Пароль"
+                      onChangeText={setPassword}
+                      keyboardType="numeric"
+                      value={password}
+                      autoCompleteType="off"
+                    />
+                    <TouchableOpacity
+                      style={styles.showPasswordLink}
+                      onPress={() => {
+                        console.log("Показати");
+                      }}
+                    >
+                      <Text style={styles.showPasswordText}>Показати</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+                {!isKeyboardOpen && (
+                <View style={styles.btnWrap}>
+                  <TouchableOpacity
+                    style={styles.submitBtn}
+                    onPress={() => {
+                      console.log("Зареєстуватися");
+                    }}
+                  >
+                    <Text style={styles.btnTitle}>Зареєстуватися</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={styles.loginLink}
+                    onPress={() => {
+                      console.log("Вже є акаунт? Увійти");
+                    }}
+                  >
+                    <Text style={styles.loginLinkText}>
+                      Вже є акаунт? Увійти
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+                )}
+                {/* <Image source={homeSvg} /> */}
+                {/* <StatusBar style="auto" /> */}
+              </View>
+            </TouchableWithoutFeedback>
+          </KeyboardAvoidingView>
+        </ImageBackground>
+      </TouchableWithoutFeedback>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    width: "100%",
+    maxWidth: 375,
+    heigth: "100%",
+    alignSelf: "center",
+  },
+  backgroundImg: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
   form: {
-    paddingTop: 92,
+    position: "relative",
     paddingLeft: 16,
     paddingRight: 16,
     backgroundColor: "#ffffff",
-    borderRadius: 25,
-    width: 375,
-    height: 549,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    ...Platform.select({
+      ios: {
+        paddingTop: 92,
+        paddingBottom: 32,
+      },
+      android: {
+        paddingTop: 70,
+        paddingBottom: 10,
+      },
+    }),
+  },
+  userImgWrap: {
+    position: "absolute",
+    borderRadius: 16,
+    backgroundColor: "#F6F6F6",
+    left: 127,
+    right: 128,
+    ...Platform.select({
+      ios: {
+        top: -60,
+        width: 120,
+        height: 120,
+      },
+      android: {
+        top: -55,
+        width: 110,
+        height: 110,
+      },
+    }),
+  },
+  iconControlUserPhoto: {
+    position: "absolute",
+    right: -12,
+    bottom: 14,
+  },
+  textWrap: {
+    marginLeft: "auto",
+    marginRight: "auto",
   },
   inputsWrap: {
     display: "flex",
-    gap: 16,
-    marginTop: 32,
-    marginBottom: 43,
-  },
-  textWrap: {
-    display: "block",
-    marginLeft: "auto",
-    marginRight: "auto",
+    ...Platform.select({
+      ios: {
+        gap: 16,
+        marginTop: 32,
+      },
+      android: {
+        gap: 10,
+        marginTop: 16,
+      },
+    }),
   },
   title: {
     fontFamily: "Roboto500",
     color: "#212121",
     fontWeight: 500,
-    fontSize: 30,
-    lineHeight: 3.17,
     letterSpacing: 0.01,
+    fontSize: 30,
   },
   input: {
     backgroundColor: "#F6F6F6",
@@ -116,49 +219,66 @@ const styles = StyleSheet.create({
     placeholderTextColor: "#BDBDBD",
   },
   passwordInputWrap: {
-    flex: 1,
     position: "relative",
   },
   showPasswordLink: {
     position: "absolute",
-    right: "5%",
-    bottom: "50%",
+    top: "25%",
+    right: "3%",
+    bottom: "25%",
   },
   showPasswordText: {
     fontFamily: "Roboto400",
-    fontWeight: "Regular",
     fontSize: 16,
-    lineHeight: 1.19,
     color: "#1B4371",
   },
-  button: {
+  btnWrap: {
+    ...Platform.select({
+      ios: {
+        marginTop: 43,
+      },
+      android: {
+        marginTop: 33,
+      },
+    }),
+  },
+  submitBtn: {
     backgroundColor: "#FF6C00",
     borderRadius: 100,
-    paddingTop: 16,
-    paddingBottom: 16,
-    paddingLeft: 32,
-    paddingRight: 32,
-    marginBottom: 16,
+    ...Platform.select({
+      ios: {
+        height: 51,
+        paddingTop: 16,
+        paddingBottom: 16,
+      },
+      android: {
+        height: 44,
+        paddingTop: 12,
+        paddingBottom: 12,
+      },
+    }),
   },
-  buttonTitle: {
-    fontFamily: "Roboto400",
-    fontWeight: 400,
+  btnTitle: {
     fontSize: 16,
-    lineHeight: 1.19,
     letterSpacing: 0.01,
     textAlign: "center",
     color: "#FFFFFF",
   },
-  link: {
-    display: "block",
+  loginLink: {
     marginLeft: "auto",
     marginRight: "auto",
+    marginTop: 16,
+    ...Platform.select({
+      ios: {
+        marginBottom: 45,
+      },
+      android: {
+        marginBottom: 10,
+      },
+    }),
   },
-  linkText: {
-    fontFamily: "Roboto400",
-    fontWeight: 400,
+  loginLinkText: {
     fontSize: 16,
-    lineHeight: 1.19,
     textAlign: "center",
     color: "#1B4371",
   },
